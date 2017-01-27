2f7ae7a97be31520f6c8a2f1133ae3573cb1d8c8
AlbertMorenoDEV <albert.moreno.dev@gmail.com>
2017-01-27 23:53:27 +0100

    Fixed product repository error

--- src/AlbertMorenoDEV/Catalog/Domain/Model/ProductRepository.php
+++ src/AlbertMorenoDEV/Catalog/Domain/Model/ProductRepository.php
@@ -6,9 +6,9 @@ use AMD\Catalog\Domain\Model\Product\ProductId;
 interface ProductRepository
 {
     public function nextIdentity(): ProductId;
-    public function add(Product $product): void;
-    public function update(Product $product): void;
-    public function remove(Product $product): void;
+    public function add(Product $product);
+    public function update(Product $product);
+    public function remove(Product $product);
     public function findAll();
     public function findByProductId(ProductId $productId);
 }
--- src/AlbertMorenoDEV/Catalog/Infrastructure/Persistence/Doctrine/DoctrineProductRepository.php
+++ src/AlbertMorenoDEV/Catalog/Infrastructure/Persistence/Doctrine/DoctrineProductRepository.php
@@ -13,18 +13,18 @@ class DoctrineProductRepository extends EntityRepository implements ProductRepos
         return ProductId::create();
     }
 
-    public function add(Product $product): void
+    public function add(Product $product)
     {
         $this->_em->persist($product);
         $this->_em->flush(); // ToDo: Must be moved to Application layer, but where exactly?
     }
 
-    public function update(Product $product): void
+    public function update(Product $product)
     {
         $this->_em->flush(); // ToDo: Must be moved to Application layer, but where exactly?
     }
 
-    public function remove(Product $product): void
+    public function remove(Product $product)
     {
         $this->_em->remove($product);
         $this->_em->flush(); // ToDo: Must be moved to Application layer, but where exactly?
